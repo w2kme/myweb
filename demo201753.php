8476c5183163020fab00a6733de6dc0275314277
w2kme <w2kme@163.com>
2017-05-04 11:39:01 +0800

    201754_01

--- demo201753.php
+++ demo201753.php
@@ -1,11 +1,8 @@
 <?php 
 	header("content-type:text/html;charset=utf-8;");
-	$name='ppp'; 
-	echo   "<br/>$name";
-	echo   '<br/>$name';//单引号不可以输出变量
-	echo   "<br/>{$name}";
+	//1493864725，
+	echo rand(0,9);
 
-	define('PAI',3.14);
-	echo PAI*3;
+
 ?>
 
